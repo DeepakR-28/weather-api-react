4871f8cd95723e37e74259461e9f8e36919c4b8f
deepakR-28 <awsmyo1234@gmail.com>
2021-06-02 12:48:35 +0530

    'init'

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     city : "Delhi",
     min : 0,
     max : 0,
-    error : "Somehting"
+    error : null
   }
   inputHandler = (event) =>{
     console.log(event.target.value)  
@@ -19,22 +19,22 @@ class App extends Component {
   }
   updateCityName = () =>{
     let city = this.state.city
-    // axios.get('https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=c5888ccbcf7a23e7faceecea8ee3b795')
-    //   .then(response =>{
-    //     console.log(response)
-    //     this.setState({
-    //       min : response.data.main.temp_min,
-    //       max : response.data.main.temp_max,
-    //       error:null
-    //     })
+    axios.get('https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=c5888ccbcf7a23e7faceecea8ee3b795')
+      .then(response =>{
+        console.log(response)
+        this.setState({
+          min : response.data.main.temp_min,
+          max : response.data.main.temp_max,
+          error:null
+        })
 
-    //   })
-    //   .catch(err =>{
-    //     console.log(err)
-    //     this.setState({
-    //       error : "We don't have this location's data"
-    //     })
-    //   })
+      })
+      .catch(err =>{
+        console.log(err)
+        this.setState({
+          error : "We don't have this location's data"
+        })
+      })
   }
   render() {
     return (
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,89 +1,19 @@
 import React from 'react'
 
+import classes from './Main.css'
 const Main = (props) =>{
-    const container = {
-        color: "black",
-        backgroundColor: "#eee",
-        padding: "10px",
-        fontFamily: "Arial",
-        width: "60%",
-        height : "80%",
-        margin: "0",
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        boxShadow : "0 4px 5px grey"
-      };
-    const input ={
-        width: "80%",
-        height: "30px",
-        margin: "0",
-        display : "block",
-        marginRight: "auto",
-        marginLeft: "auto",
-        marginTop : "5%",
-        border : "1px solid #ccc",
-        fontFamily : "'Roboto', sans-serif",
-        fontSize : "20px",
-    }
-    const label = {
-        margin: "0",
-        display : "block",
-        marginRight: "auto",
-        marginLeft: "auto",
-        textAlign : "center",
-        fontFamily: "'Roboto', sans-serif",
-        fontSize : "30px"
-    }
-    const button = {
-        display: "block",
-        marginLeft : "auto",
-        marginRight: "auto",
-        fontFamily: "'Roboto', sans-serif",
-        color: "black",
-        fontSize : "20px",
-        backgroundColor : "#f39c12",
-        border : "0px solid transparent",
-        width: "100px",
-        height : "40px",
-        borderRadius :"10px"
-    }
-    const temperatureContainer = {
-        // backgroundColor : "#f39c12",
-        // margin: "0",
-        // position: "absolute",
-        // left: "50%",
-        // width: "10%",
-        // height:""
-        display: "flex",
-        flexDirection : "row",
-    }
-    const temp ={
-        width : "200px",
-        height : "100px",
-        backgroundColor : "#ecf0f1",
-        margin: "10% 20% 0% 20%",
-        fontFamily: "'Roboto', sans-serif",
-        fontSize:"20px"
-    }
-    const error ={
-        margin :"0",
-        textAlign : "center",
-        color: "Red",
-        marginTop: "20px"
-    }
+
     return (
-        <div style = {container}>
-            <label style = {label}>Enter City</label>
+        <div className = {classes.container}>
+            <label className = {classes.label}>Enter City</label>
             <br></br>
-            <input style = {input} onChange={props.input} placeholder = "ex : Delhi"/>
+            <input className = {classes.input} onChange={props.input} placeholder = "ex : Delhi"/>
             <br></br>
-            <button style = {button} onClick = {props.searchHandler}>Search</button>
-            <p style = {error}>{props.error}</p>
-            <div style = {temperatureContainer}>
-                <div style = {temp}><strong>Max</strong><br />{props.maxTemp}</div>
-                <div style = {temp}><strong>Min</strong><br />{props.minTemp}</div>
+            <button className = {classes.button} onClick = {props.searchHandler}>Search</button>
+            <p className = {classes.error}>{props.error}</p>
+            <div className = {classes.temperatureContainer}>
+                <div className = {classes.temp}><strong>Max</strong><br />{props.maxTemp}</div>
+                <div className = {classes.temp}><strong>Min</strong><br />{props.minTemp}</div>
             </div>
         </div>
     )
